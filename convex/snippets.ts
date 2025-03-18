927e108f397e7753090f6b2d8fcad1b72afa8a8f
Himansu <himansuthapa2059@gmail.com>
2025-03-18 22:40:13 +0545

    Feat: add queries for retrieving snippets and starred snippets

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const createSnippet = mutation({
   args: {
@@ -29,5 +29,51 @@ export const createSnippet = mutation({
     });
 
     return snippetId;
+  },
+});
+
+export const getSnippets = query({
+  handler: async (ctx) => {
+    const snippes = await ctx.db.query("snippets").order("desc").collect();
+    return snippes;
+  },
+});
+
+export const isSnippetStarred = query({
+  args: { snippetId: v.id("snippets") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Not authenticated");
+
+    const star = await ctx.db
+      .query("stars")
+      .withIndex("by_user_id_and_snippet_id")
+      .filter(
+        (q) =>
+          q.eq(q.field("userId"), identity.subject) &&
+          q.eq(q.field("snippetId"), args.snippetId)
+      )
+      .first();
+
+    return !!star;
+  },
+});
+
+export const getStarredSnippets = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) return [];
+
+    const stars = await ctx.db
+      .query("stars")
+      .withIndex("by_user_id")
+      .filter((q) => q.eq(q.field("userId"), identity.subject))
+      .collect();
+
+    const snippets = await Promise.all(
+      stars.map((star) => ctx.db.get(star.snippetId))
+    );
+
+    return snippets.filter((snippet) => snippet !== null);
   },
 });
